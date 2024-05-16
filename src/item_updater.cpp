2f72d1687b867b5039fce08eb309d50efa0ad70f
Faisal Awada <faisal@us.ibm.com>
2024-05-16 13:31:32 -0500

    Waiting for PSUs to be available during startup
    Modified the Software PSU Updater Service to wait until inventory
    service available. Updated ItemUpdater::scanDirectory to search
    for the installed PSU model directory. For example, the PSU model
    2B1E should have a directory named /usr/share/obmc/psu/2B1E. The
    firmware file and the MANIFEST reside in the model directory.
    
    Added a new function, `onInterfacesAdded`, processes D-Bus messages
    to update the internal state of PSU  devices. It performs the following
    steps:
    - Read D-Bus message to extract to extract the object path and
      interfaces.
    - Check for PSU Interface.
    - Retrieve all PSUs inventory paths from the D-Bus.
    - Update PSU present status and model. If the PSU is present and has
      valid version, process FW update to latest version.
    
    Tested:
    Tested the code in simulation module and verified it waits for all PSUs
    to be discovered. Verified phosphor-psu-code-manager invokes psutils to
    get the PSU version, compare and update.
    
    Change-Id: Ic26e215baffd56fc070cc0cf6d3fff92fdfb914c
    Signed-off-by: Faisal Awada <faisal@us.ibm.com>

--- src/item_updater.cpp
+++ src/item_updater.cpp
@@ -391,6 +391,8 @@ void ItemUpdater::processPSUImage()
         auto service = utils::getService(bus, p.c_str(), ITEM_IFACE);
         auto present = utils::getProperty<bool>(bus, service.c_str(), p.c_str(),
                                                 ITEM_IFACE, PRESENT);
+        psuStatusMap[p].model = utils::getProperty<std::string>(
+            bus, service.c_str(), p.c_str(), ASSET_IFACE, MODEL);
         auto version = utils::getVersion(p);
         if (present && !version.empty())
         {
@@ -411,11 +413,13 @@ void ItemUpdater::processPSUImage()
 void ItemUpdater::processStoredImage()
 {
     scanDirectory(IMG_DIR_BUILTIN);
+
     scanDirectory(IMG_DIR_PERSIST);
 }
-
 void ItemUpdater::scanDirectory(const fs::path& dir)
 {
+    auto manifest = dir;
+    auto path = dir;
     // The directory shall put PSU images in directories named with model
     if (!fs::exists(dir))
     {
@@ -428,28 +432,47 @@ void ItemUpdater::scanDirectory(const fs::path& dir)
                         entry("PATH=%s", dir.c_str()));
         return;
     }
-    for (const auto& d : fs::directory_iterator(dir))
+
+    for (const auto& [key, item] : psuStatusMap)
     {
-        // If the model in manifest does not match the dir name
-        // Log a warning and skip it
-        auto path = d.path();
-        auto manifest = path / MANIFEST_FILE;
-        if (fs::exists(manifest))
+        if (!item.model.empty())
         {
-            auto ret = Version::getValues(
-                manifest.string(),
-                {MANIFEST_VERSION, MANIFEST_EXTENDED_VERSION});
-            auto version = ret[MANIFEST_VERSION];
-            auto extVersion = ret[MANIFEST_EXTENDED_VERSION];
-            auto info = Version::getExtVersionInfo(extVersion);
-            auto model = info["model"];
-            if (path.stem() != model)
-            {
-                log<level::ERR>("Unmatched model",
-                                entry("PATH=%s", path.c_str()),
-                                entry("MODEL=%s", model.c_str()));
-                continue;
-            }
+            path = path / item.model;
+            manifest = dir / item.model / MANIFEST_FILE;
+            break;
+        }
+    }
+
+    if (!fs::is_directory(path))
+    {
+        log<level::ERR>("The path is not a directory",
+                        entry("PATH=%s", path.c_str()));
+        return;
+    }
+
+    if (!fs::exists(manifest))
+    {
+        log<level::ERR>("No MANIFEST found",
+                        entry("PATH=%s", manifest.c_str()));
+        return;
+    }
+    // If the model in manifest does not match the dir name
+    // Log a warning
+    if (fs::is_regular_file(manifest))
+    {
+        auto ret = Version::getValues(
+            manifest.string(), {MANIFEST_VERSION, MANIFEST_EXTENDED_VERSION});
+        auto version = ret[MANIFEST_VERSION];
+        auto extVersion = ret[MANIFEST_EXTENDED_VERSION];
+        auto info = Version::getExtVersionInfo(extVersion);
+        auto model = info["model"];
+        if (path.stem() != model)
+        {
+            log<level::ERR>("Unmatched model", entry("PATH=%s", path.c_str()),
+                            entry("MODEL=%s", model.c_str()));
+        }
+        else
+        {
             auto versionId = utils::getVersionId(version);
             auto it = activations.find(versionId);
             if (it == activations.end())
@@ -474,11 +497,11 @@ void ItemUpdater::scanDirectory(const fs::path& dir)
                 it->second->path(path);
             }
         }
-        else
-        {
-            log<level::ERR>("No MANIFEST found",
-                            entry("PATH=%s", path.c_str()));
-        }
+    }
+    else
+    {
+        log<level::ERR>("MANIFEST is not a file",
+                        entry("PATH=%s", manifest.c_str()));
     }
 }
 
@@ -537,6 +560,42 @@ void ItemUpdater::invokeActivation(
     activation->requestedActivation(Activation::RequestedActivations::Active);
 }
 
+void ItemUpdater::onInterfacesAdded(sdbusplus::message_t& msg)
+{
+    sdbusplus::message::object_path objPath;
+    std::map<std::string,
+             std::map<std::string, std::variant<bool, std::string>>>
+        interfaces;
+    msg.read(objPath, interfaces);
+    std::string path = static_cast<std::string>(objPath);
+
+    if (interfaces.find(PSU_INVENTORY_IFACE) == interfaces.end())
+    {
+        return;
+    }
+
+    auto paths = utils::getPSUInventoryPath(bus);
+
+    sleep(1);
+    for (const auto& p : paths)
+    {
+        auto service = utils::getService(bus, p.c_str(), ITEM_IFACE);
+        psuStatusMap[p].present = utils::getProperty<bool>(
+            bus, service.c_str(), p.c_str(), ITEM_IFACE, PRESENT);
+
+        service = utils::getService(bus, p.c_str(), ASSET_IFACE);
+        psuStatusMap[p].model = utils::getProperty<std::string>(
+            bus, service.c_str(), p.c_str(), ASSET_IFACE, MODEL);
+
+        auto version = utils::getVersion(p);
+        if (psuStatusMap[p].present && !version.empty())
+        {
+            processPSUImage();
+            processStoredImage();
+            syncToLatestImage();
+        }
+    }
+}
 } // namespace updater
 } // namespace software
 } // namespace phosphor
